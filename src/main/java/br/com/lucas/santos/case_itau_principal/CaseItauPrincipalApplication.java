ba40f3194e815a6e4c06777c9832325d3ece84c1
Lucas <lucasvinipro23@gmail.com>
2025-04-17 21:50:23 -0300

    criação do projeto

---
+++ src/main/java/br/com/lucas/santos/case_itau_principal/CaseItauPrincipalApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.santos.case_itau_principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseItauPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseItauPrincipalApplication.class, args);
+	}
+
+}
